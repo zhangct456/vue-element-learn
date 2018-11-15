84dc211f6f052a81d9eb674b5812fa733d847253
zhangct456 <zhangct456@163.com>
2018-11-15 17:45:34 +0800

    资源加载顺序

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
 import router from './router'
 import setting from './setting'
 import store from './store'
+import App from './App'
 import "@/assets/js/jquery-1.9.1.js"
+
 import "@/assets/css/commen.css"
 import "@/assets/css/markdown.css"
 
