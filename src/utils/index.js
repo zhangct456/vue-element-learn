6e4bfef1a405956383f535dbfa8157ec246d1ecc
zhangct456 <zhangct456@163.com>
2018-11-23 17:32:19 +0800

    增加处理md文件代码块高亮方法

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,10 +1,11 @@
 import routerCreate from "./routerCreate"
 import getSubMenu from "./menuList"
 import { fnScroll } from "./scroll"
-
+import highlightCode from "./highlight"
 
 export {
 	routerCreate,
 	getSubMenu,
-	fnScroll
+	fnScroll,
+	highlightCode
 }
