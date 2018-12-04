32e1f64b32544183c07c5c9551b36dad0b0af04e
zhangct456 <zhangct456@163.com>
2018-12-04 17:10:17 +0800

    新增笔记目录

---
+++ src/pages/Notes/Gulp/Gulp.md
@@ -0,0 +1,34 @@
+## gulp
+
+> API文档[https://www.gulpjs.com.cn/docs/api/](https://www.gulpjs.com.cn/docs/api/)
+
+##### 安装
+
+需要全局安装，只安装本地gulp，命令行无法执行`gulp -v`，也无法执行gulp其它的相关命令。
+
+```
+npm install -g gulp
+```
+
+----
+
+##### 打包zip遇到的问题
+
+压缩代码之后打包成zip，使用回调函数来控制执行顺序：
+
+1.报错；
+
+2.不报错，但是没有生成zip包
+
+设置一个延时之后执行结果正常。
+
+```
+gulp.task("zip", ["del", "minifyJs", "minifyCss", "copy", "minifyHtml", "minifyIndex"], function(cb) {
+	setTimeout(function(){
+		gulp.src('dist/*.*')
+			.pipe(zip('dist.zip'))
+			.pipe(gulp.dest("dist"));
+		cb();
+	}, 2000)
+});
+```
---
+++ src/pages/Notes/Mod.vue
@@ -0,0 +1,38 @@
+<template>
+	<el-container>
+		<el-aside class="aside-box" width="200px">
+			<Aside sub-menu-name="notes"></Aside>
+		</el-aside>
+		<el-main>
+			<router-view/>
+		</el-main>
+	</el-container>
+</template>
+
+<script>
+	import Aside from "@/components/Aside.vue"
+	export default {
+		name: "Notes",
+		components: {
+			Aside
+		},
+		data() {
+			return {
+
+			}
+		},
+		props: [],
+		computed: {},
+		watch: {},
+		methods: {},
+		beforeCreated() {},
+		created() {},
+		mounted() {}
+	}
+</script>
+
+<style>
+.aside-box{
+	background-color: #545c64;
+}
+</style>
