8db1eb6813cefd7f3a527d68696cf23d24f6c62e
zhangct456 <zhangct456@163.com>
2018-12-06 10:15:54 +0800

    配置转发

--- config/index.js
+++ config/index.js
@@ -10,7 +10,15 @@ module.exports = {
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+    	"/test": {
+				target: "http://127.0.0.1:8099",
+				changeOrigin: true,
+				pathRewrite: {
+					"^/test": ""
+				}
+			},
+    },
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
